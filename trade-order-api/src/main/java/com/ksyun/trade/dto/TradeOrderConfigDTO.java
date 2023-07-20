a7933ad28b35f8a71759a5d166f474a14032cd78
libo14 <libo14@kingsoft.com>
2023-07-20 23:08:12 +0800

    add:添加了查询user信息的类

---
+++ trade-order-api/src/main/java/com/ksyun/trade/dto/TradeOrderConfigDTO.java
@@ -0,0 +1,17 @@
+package com.ksyun.trade.dto;
+
+import lombok.Data;
+import org.apache.ibatis.annotations.Select;
+
+import java.io.Serializable;
+
+@Data
+public class TradeOrderConfigDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int user_id;
+
+    private int region_id;
+
+    private double price_value;
+}
