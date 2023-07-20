c3d8f1f8f1ab798adbe016e5475a4d712c6701dd
libo14 <libo14@kingsoft.com>
2023-07-20 16:31:30 +0800

    feat:初步实现了查询用户信息

--- trade-order-api/src/main/java/com/ksyun/trade/service/TradeOrderService.java
+++ trade-order-api/src/main/java/com/ksyun/trade/service/TradeOrderService.java
@@ -1,7 +1,10 @@
 package com.ksyun.trade.service;
 
+import com.ksyun.common.util.mapper.JacksonMapper;
+import com.ksyun.trade.rest.RestResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
@@ -9,7 +12,13 @@ public class TradeOrderService {
 
     public Object query(Integer id) {
         //TODO
-        return null;
+        RestTemplate restTemplate = new RestTemplate();
+
+//        String str = ;
+        RestResult restResult = restTemplate.getForObject("http://campus.meta.ksyun.com:8090/online/user/{id}", RestResult.class, id);//JacksonMapper.defaultMapper().fromJson(str, RestResult.class);
+
+
+        return restResult.getData();
     }
 
 }
--- trade-order-gateway/src/main/java/com/ksyun/trade/service/GatewayService.java
+++ trade-order-gateway/src/main/java/com/ksyun/trade/service/GatewayService.java
@@ -1,16 +1,40 @@
 package com.ksyun.trade.service;
 
+import com.ksyun.common.util.mapper.JacksonMapper;
 import com.ksyun.trade.rest.RestResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.HttpRequestHandler;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.ksyun.common.util.mapper.JacksonMapper.defaultMapper;
 
 @Service
 public class GatewayService {
 
+    @Autowired
+    private HttpRequestHandler httpRequestHandler;
+
     public Object loadLalancing(Object param) {
         // 1. 模拟路由 (负载均衡) 获取接口
-
         // 2. 请求转发
-        return null;
+
+        RestTemplate restTemplate = new RestTemplate();
+        RestResult<HashMap<String, Object>> restResult = restTemplate.getForObject("http://localhost:8089/online/trade_order/{id}", RestResult.class, param);
+        restResult.setRequestId("");
+        HashMap<String, Object> map = restResult.getData();
+        map.remove("id");
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("id",param);
+        data.put("priceValue",9406.5013);
+        data.put("user",map);
+        restResult.setData(data);
+
+        return restResult;
     }
 
 
