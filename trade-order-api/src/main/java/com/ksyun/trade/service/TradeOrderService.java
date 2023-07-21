ced7a6b62648bb8919279ceb565a13374c93613f
hot--water <10209501+hot--water@user.noreply.gitee.com>
2023-07-21 14:58:25 +0800

    refacter:使用TradeSelectUtil分离功能

--- trade-order-api/src/main/java/com/ksyun/trade/service/TradeOrderService.java
+++ trade-order-api/src/main/java/com/ksyun/trade/service/TradeOrderService.java
@@ -1,20 +1,15 @@
 package com.ksyun.trade.service;
 
-import com.ksyun.trade.dto.TradeOrderConfigDTO;
+import com.ksyun.trade.dto.TradeOrderDTO;
 import com.ksyun.trade.dto.TradeProductConfigDTO;
-import com.ksyun.trade.mapper.TradeMapper;
 import com.ksyun.trade.rest.RestResult;
+import com.ksyun.trade.util.TradeSelectUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.InputStream;
 import java.util.HashMap;
 
 @Service
@@ -26,19 +21,13 @@ public class TradeOrderService {
     private HttpServletRequest request;
 
 
-    public RestResult query(Integer id) throws Exception {
+    public RestResult query(Integer id) {
         //TODO
-        RestTemplate restTemplate = new RestTemplate();
-        String resource = "mybatis-config.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        TradeMapper tradeMapper = sqlSession.getMapper(TradeMapper.class);
-        TradeOrderConfigDTO tocd = tradeMapper.selectUserById(id);
+        TradeOrderDTO tocd = TradeSelectUtil.selectOrderById(id);
         int userId = tocd.getUser_id();
+        TradeProductConfigDTO[] tpcd = TradeSelectUtil.selectProductById(id);
 
-        TradeProductConfigDTO[] tpcd = tradeMapper.selectProductById(id);
-
+        RestTemplate restTemplate = new RestTemplate();
         RestResult<HashMap<String, Object>> restResult = restTemplate.getForObject(
                 "http://campus.meta.ksyun.com:8090/online/user/{id}", RestResult.class, userId);
 
