b5fcb0ca9c9bff6ef8fae8ed200701546172b79f
hot--water <10209501+hot--water@user.noreply.gitee.com>
2023-07-21 16:03:56 +0800

    refactor:优化import

--- trade-order-api/src/main/java/com/ksyun/trade/controller/online/TradeOrderController.java
+++ trade-order-api/src/main/java/com/ksyun/trade/controller/online/TradeOrderController.java
@@ -1,13 +1,25 @@
 package com.ksyun.trade.controller.online;
 
+import com.google.common.collect.Table;
+import com.ksyun.trade.mapper.TradeMapper;
 import com.ksyun.trade.rest.RestResult;
 import com.ksyun.trade.service.TradeOrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping(value = "/online/trade_order", produces = {MediaType.APPLICATION_JSON_VALUE})
