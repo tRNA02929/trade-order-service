c98c507e7c66121bdd962ecbd675ee1c5b7c89ab
hot--water <10209501+hot--water@user.noreply.gitee.com>
2023-07-21 15:26:39 +0800

    feat:在data中添加了region信息，但未做优化

--- trade-order-api/src/main/java/com/ksyun/trade/service/TradeOrderService.java
+++ trade-order-api/src/main/java/com/ksyun/trade/service/TradeOrderService.java
@@ -1,5 +1,6 @@
 package com.ksyun.trade.service;
 
+import com.google.common.collect.Maps;
 import com.ksyun.trade.dto.TradeOrderDTO;
 import com.ksyun.trade.dto.TradeProductConfigDTO;
 import com.ksyun.trade.rest.RestResult;
@@ -10,7 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @Slf4j
@@ -24,20 +27,37 @@ public class TradeOrderService {
     public RestResult query(Integer id) {
         //TODO
         TradeOrderDTO tocd = TradeSelectUtil.selectOrderById(id);
-        int userId = tocd.getUser_id();
         TradeProductConfigDTO[] tpcd = TradeSelectUtil.selectProductById(id);
+        int userId = tocd.getUser_id();
+        int regionId = tocd.getRegion_id();
 
         RestTemplate restTemplate = new RestTemplate();
-        RestResult<HashMap<String, Object>> restResult = restTemplate.getForObject(
+        RestResult<HashMap<String, Object>> userResult = restTemplate.getForObject(
                 "http://campus.meta.ksyun.com:8090/online/user/{id}", RestResult.class, userId);
+        RestResult<ArrayList<HashMap<String, Object>>> redionResult = restTemplate.getForObject(
+                "http://campus.meta.ksyun.com:8090/online/region/list", RestResult.class);
 
-        HashMap<String, Object> map = restResult.getData();
+
+        HashMap<String, Object> map = userResult.getData();
         map.remove("id");
         HashMap<String, Object> data = new HashMap<>();
+        data.put("id", id);
         data.put("priceValue", tocd.getPrice_value());
-        data.put("id", userId);
         data.put("user", map);
+
+        for (HashMap<String, Object> region : redionResult.getData()) {
+            if (regionId == (int) region.get("id")) {
+                map = new HashMap<>();
+                map.put("code", region.get("code"));
+                map.put("name", region.get("name"));
+                data.put("region", map);
+                break;
+            }
+        }
+
         data.put("configs", tpcd);
+
+        RestResult restResult = new RestResult();
         restResult.setData(data);
         return restResult;
     }
