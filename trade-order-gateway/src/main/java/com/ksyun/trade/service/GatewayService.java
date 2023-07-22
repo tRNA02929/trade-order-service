afed966810c0c628df1a5fc464cc58496733366d
libo14 <libo14@kingsoft.com>
2023-07-22 22:22:36 +0800

    feat:为订单查询，机房查询，优惠券抵扣添加单独的接口

--- trade-order-gateway/src/main/java/com/ksyun/trade/service/GatewayService.java
+++ trade-order-gateway/src/main/java/com/ksyun/trade/service/GatewayService.java
@@ -1,6 +1,7 @@
 package com.ksyun.trade.service;
 
 import com.ksyun.common.util.mapper.JacksonMapper;
+import com.ksyun.trade.dto.VoucherDeductDTO;
 import com.ksyun.trade.rest.RestResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,12 +28,29 @@ public class GatewayService {
 //    @Value("${actions}")
 //    private String[] actions;
 
-    public Object loadLalancing(Object param) throws IOException {
+    public Object queryOrderInfo(Object param) throws IOException {
         // 1. 模拟路由 (负载均衡) 获取接口
         // 2. 请求转发
-        response.sendRedirect("http://localhost:8089" + "/online/trade_order/" + param);
+        response.sendRedirect("http://localhost:8089/online/trade_order/" + param);
         return null;
     }
 
+    public Object queryRegionName(Object param) throws IOException {
+        // 1. 模拟路由 (负载均衡) 获取接口
+        // 2. 请求转发
+        response.sendRedirect("http://localhost:8089/online/trade_order/region/" + param);
+        return null;
+    }
 
+    public Object loadLalancing(Object param) throws IOException {
+        // 1. 模拟路由 (负载均衡) 获取接口
+        // 2. 请求转发
+        response.sendRedirect("http://localhost:8089/online/trade_order/" + param);
+        return null;
+    }
+
+    public Object deduct(VoucherDeductDTO param) throws IOException {
+        response.sendRedirect("http://localhost:8089/online/trade_order/deduct?" + param);
+        return null;
+    }
 }
