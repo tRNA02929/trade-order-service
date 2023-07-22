5ae6b49d62263d9dda05ca36620f42ae522dc19f
libo14 <libo14@kingsoft.com>
2023-07-22 22:32:58 +0800

    feat:添加了不同的接口供调用

--- trade-order-api/src/main/java/com/ksyun/trade/controller/online/TradeOrderController.java
+++ trade-order-api/src/main/java/com/ksyun/trade/controller/online/TradeOrderController.java
@@ -1,25 +1,14 @@
 package com.ksyun.trade.controller.online;
 
-import com.google.common.collect.Table;
-import com.ksyun.trade.mapper.TradeMapper;
+import com.ksyun.trade.dto.VoucherDeductDTO;
 import com.ksyun.trade.rest.RestResult;
 import com.ksyun.trade.service.TradeOrderService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 @RestController
 @RequestMapping(value = "/online/trade_order", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -29,8 +18,17 @@ public class TradeOrderController {
     private TradeOrderService orderService;
 
     @RequestMapping("/{id}")
-    public RestResult query(@PathVariable("id") Integer id) throws Exception {
-        return orderService.query(id);
+    public RestResult queryOrderInfo(@PathVariable("id") Integer id) {
+        return orderService.queryOrderInfo(id);
     }
 
+    @RequestMapping("/region/{id}")
+    public RestResult queryRegionName(@PathVariable("id") Integer id) {
+        return orderService.queryRegionName(id);
+    }
+
+    @RequestMapping("/deduct")
+    public RestResult deduct(VoucherDeductDTO param) {
+        return orderService.deduct(param);
+    }
 }
