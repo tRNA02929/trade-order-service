e6eb97e0d07be737e7f1dfe41b216115e02d36c3
libo14 <libo14@kingsoft.com>
2023-07-22 22:23:40 +0800

    feat:使用修改后的接口作为实现

--- trade-order-gateway/src/main/java/com/ksyun/trade/controller/online/GatewayController.java
+++ trade-order-gateway/src/main/java/com/ksyun/trade/controller/online/GatewayController.java
@@ -3,6 +3,7 @@ package com.ksyun.trade.controller.online;
 import com.ksyun.trade.dto.VoucherDeductDTO;
 import com.ksyun.trade.service.GatewayService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import javax.servlet.ServletException;
 import java.io.IOException;
 
 @RestController
+@RequestMapping(value = "/online", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class GatewayController {
     @Autowired
     private GatewayService gatewayService;
@@ -18,31 +20,31 @@ public class GatewayController {
     /**
      * 查询订单详情 (GET)
      */
-    @RequestMapping(value = "/online/queryOrderInfo", produces = "application/json")
+    @RequestMapping(value = "/queryOrderInfo", produces = "application/json")
     public Object queryOrderInfo(Integer id) throws IOException {
-        return gatewayService.loadLalancing(id);
+        return gatewayService.queryOrderInfo(id);
     }
 
     /**
      * 根据机房Id查询机房名称 (GET)
      */
-    @RequestMapping(value = "/online/queryRegionName", produces = "application/json")
+    @RequestMapping(value = "/queryRegionName", produces = "application/json")
     public Object queryRegionName(Integer regionId) throws IOException {
-        return gatewayService.loadLalancing(regionId);
+        return gatewayService.queryRegionName(regionId);
     }
 
     /**
      * 订单优惠券抵扣 (POST json)
      */
-    @RequestMapping(value = "/online/deduct", produces = "application/json")
-    public Object deduct(@RequestBody VoucherDeductDTO param) throws IOException {
-        return gatewayService.loadLalancing(param);
+    @RequestMapping(value = "/deduct", produces = "application/json")
+    public Object deduct(VoucherDeductDTO param) throws IOException {
+        return gatewayService.deduct(param);
     }
 
     /**
      * 基于Redis实现漏桶限流算法，并在API调用上体现
      */
-    @RequestMapping(value = "/online/listUpstreamInfo", produces = "application/json")
+    @RequestMapping(value = "/listUpstreamInfo", produces = "application/json")
     public Object listUpstreamInfo() {
         return null;
     }
