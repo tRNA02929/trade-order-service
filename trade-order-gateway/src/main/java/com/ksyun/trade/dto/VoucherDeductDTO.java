c0884034d10cb2c025ef9dd0d3427537736eb78b
libo14 <libo14@kingsoft.com>
2023-07-23 23:07:50 +0800

    refactor:取消了对于beforeDeductAmount和afterDeductAmount的注释，同步更改了toString

--- trade-order-gateway/src/main/java/com/ksyun/trade/dto/VoucherDeductDTO.java
+++ trade-order-gateway/src/main/java/com/ksyun/trade/dto/VoucherDeductDTO.java
@@ -2,21 +2,24 @@ package com.ksyun.trade.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Data
-public class VoucherDeductDTO {
+public class VoucherDeductDTO implements Serializable {
 
     private Integer orderId;
     private String voucherNo;
     private BigDecimal amount;
-//    private BigDecimal beforeDeductAmount;
-//    private BigDecimal afterDeductAmount;
+    private BigDecimal beforeDeductAmount;
+    private BigDecimal afterDeductAmount;
 
     @Override
     public String toString() {
         return "orderId=" + orderId + "&" +
                 "voucherNo=" + voucherNo + '&' +
-                "amount=" + amount;
+                "amount=" + amount + '&' +
+                "beforeDeductAmount=" + beforeDeductAmount + '&' +
+                "afterDeductAmount=" + afterDeductAmount;
     }
 }
