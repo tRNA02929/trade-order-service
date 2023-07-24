12e70f3e526fefb6a347c4e271c98c3b76c31551
libo14 <libo14@kingsoft.com>
2023-07-24 10:44:46 +0800

    feat:在gateway使用轮询实现了负载均衡

--- trade-order-gateway/src/main/java/com/ksyun/trade/service/GatewayService.java
+++ trade-order-gateway/src/main/java/com/ksyun/trade/service/GatewayService.java
@@ -2,8 +2,12 @@ package com.ksyun.trade.service;
 
 import com.ksyun.trade.dto.VoucherDeductDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.web.HttpRequestHandler;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -12,20 +16,40 @@ public class GatewayService {
     @Autowired
     private HttpServletResponse response;
 
-//    @Value("${actions}")
-//    private String[] actions;
+    @Value("${actions}")
+    private String[] actions;
+
+    private boolean round = false;
+
+    public String round() {
+        String action;
+        if (round) {
+            round = false;
+            action = actions[0];
+        } else {
+            round = true;
+            action = actions[1];
+        }
+        action = "http://localhost:8089";
+        return action;
+    }
 
     public Object queryOrderInfo(Object param) throws IOException {
         // 1. 模拟路由 (负载均衡) 获取接口
         // 2. 请求转发
-        response.sendRedirect("http://localhost:8089/online/trade_order/" + param);
+        response.sendRedirect(round() + "/online/trade_order/" + param);
         return null;
     }
 
     public Object queryRegionName(Object param) throws IOException {
         // 1. 模拟路由 (负载均衡) 获取接口
         // 2. 请求转发
-        response.sendRedirect("http://localhost:8089/online/trade_order/region/" + param);
+        response.sendRedirect(round() + "/online/trade_order/region/" + param);
+        return null;
+    }
+
+    public Object deduct(VoucherDeductDTO param) throws IOException {
+        response.sendRedirect(round() + "/online/trade_order/deduct?" + param);
         return null;
     }
 
@@ -36,8 +60,5 @@ public class GatewayService {
         return null;
     }
 
-    public Object deduct(VoucherDeductDTO param) throws IOException {
-        response.sendRedirect("http://localhost:8089/online/trade_order/deduct?" + param);
-        return null;
-    }
+
 }
